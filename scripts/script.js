c65ce686965c5edc13aafd0bd0839dd2e578833e
mellonfarmer <shaunbu13@yahoo.co.uk>
2019-11-14 22:51:27 +0000

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+var script = document.createElement('script');
+script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+
+function dotClick(dotNumber)
+{
+	//take dot number
+	//toggle boolen value depending on dot on or off
+	//lookup braille list
+	//update text with appropirate letter
+
+
+	//eventually have it so you can write a letter and the brallie will appear
+}

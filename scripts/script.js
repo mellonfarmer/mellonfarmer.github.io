140f69041cdffdbd419fb95f7d779fcb8cd0e620
mellonfarmer <shaunbu13@yahoo.co.uk>
2019-11-24 19:48:43 +0000

    can now get all dots for all tiles

--- scripts/script.js
+++ scripts/script.js
@@ -195,7 +195,8 @@ function dotClick(dotNumber)
 }
 class tile{
 	//give id
-	constructor(){
+	constructor()
+	{
 		this.activeDots = [false,false,false,false,false,false];
 		this.tileId = activeTiles.length;
 		this.drawTile();
@@ -206,8 +207,9 @@ class tile{
 	{
 		return this.tileId;
 	}
+
 	drawTile()
-	    {//activeTiles[this.tileId].setDots(1);
+	{//activeTiles[this.tileId].setDots(1);
 	    	$("#mainContainer").append("<div class=\"Braille-container\">\
 	    	<div class=\"Braille-dot\" id=\"Braille-dot-id-"+ this.tileId + "-1\" onclick=\"activeTiles["+this.tileId+"].setDots(1)\"></div>\
 	    	<div class=\"Braille-dot\" id=\"Braille-dot-id-"+ this.tileId + "-4\" onclick=\"activeTiles["+this.tileId+"].setDots(4)\"></div>\
@@ -216,38 +218,32 @@ class tile{
 	    	<div class=\"Braille-dot\" id=\"Braille-dot-id-"+ this.tileId + "-3\" onclick=\"activeTiles["+this.tileId+"].setDots(3)\"></div>\
 	    	<div class=\"Braille-dot\" id=\"Braille-dot-id-"+ this.tileId + "-6\" onclick=\"activeTiles["+this.tileId+"].setDots(6)\"></div>"
 	    	);
-	    }
+	}
 
 	getactiveDots(dotId)
-		{
+	{
 			return this.activeDots[dotId];
-		}
+	}
 
 	setDots(dotID)
-		{
+	{
 			//let fullPath = ("#Braille-dot-id-" + this.tileid + "-"+ dotID);
 
-			if($("#Braille-dot-id-" + this.tileId + "-"+ dotID).css("background-color") == 'rgb(255, 255, 255)'){
+		if($("#Braille-dot-id-" + this.tileId + "-"+ dotID).css("background-color") == 'rgb(255, 255, 255)')
+		{
 			$("#Braille-dot-id-" + this.tileId + "-"+ dotID).css("background-color","rgb(0, 0, 0)");
 			this.activeDots[(dotID-1)] = true;
 			console.log("Array contence: " + activeDots[(dotID-1)] + "Dot number " + dotID);
-			console.log("ON");
-
 		} 
-			else if($("#Braille-dot-id-" + this.tileId + "-"+ dotID).css("background-color") == 'rgb(0, 0, 0)'){
+
+		else if($("#Braille-dot-id-" + this.tileId + "-"+ dotID).css("background-color") == 'rgb(0, 0, 0)')
+		{
 			$("#Braille-dot-id-" + this.tileId + "-"+ dotID).css("background-color","rgb(255, 255, 255)");
 			this.activeDots[(dotID-1)] = false;
 			console.log("Array contence: " + activeDots[(dotID-1)] + "Dot number " + dotID);
-			console.log("OFF")
-			/*
-			else if ($("#Braille-dot-" + dotNumber).css("background-color") == 'rgb(0, 0, 0)'){
-			$("#Braille-dot-" + dotNumber).css("background-color","rgb(255, 255, 255)");
-			activeDots[(dotNumber-1)] = false;
-			console.log("Array contence: " + activeDots[(dotNumber-1)] + "Dot number " + dotNumber);
-			*/
+		}
 
-		}
-		}
+	}
 }
 
 /*
@@ -284,14 +280,67 @@ var tile = {
 	}*/
 //}; 
 
+let lookup = {
+	"0" : "a",
+	"01" : "b",
+	"03" : "c",
+	"034" : "d",
+	"04" : "e",
+	"013" : "f",
+	"0134" : "g",
+	"014" : "h",
+	"13" : "i",
+	"134" : "j",
 
+	"02" : "k",
+	"012" : "l",
+	"023" : "m",
+	"0234" : "n",
+	"024" : "o",
+	"0123" : "p",
+	"01234" : "q",
+	"0124" : "r",
+	"123" : "s",
+	"1234" : "t",
+
+	"025" : "u",
+	"0125" : "v",
+	"1345" : "w",
+	"0235" : "x",
+	"02345" : "y",
+	"0245" : "z",
+
+	"" : " "
+
+
+}
 
 
 function addTile()
 {
+	// activeTiles[1] = new tile;
+	activeTiles.push(new tile);
 
+}
+
+var text = "";
+var copleteText =[];
+
+function getAllDots()
+{	
+	text = "";
+	//get tile id, get dot array, output to a string
+	for (var i = 0; i <= activeTiles.length -1 ; i++) {
+		var activeDotsInCuttentTile = "";
+
+		for (var y = 0;y <= 5;y++){
+			if (activeTiles[i].getactiveDots(y) == true)
+				activeDotsInCuttentTile = activeDotsInCuttentTile + y; //+ y + " ";
+		}
+		
+		text = text + lookup[activeDotsInCuttentTile];
+		
+	}
 	
-// activeTiles[1] = new tile;
-activeTiles.push(new tile);
-
+	$("#lblOutput").text(text);
 }

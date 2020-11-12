2879eda2414fa60816e7a8106e73dd0262812e4c
mellonfarmer <shaunbu13@yahoo.co.uk>
2020-11-12 22:21:27 +0000

    cleaned up unused functions fixed dot view

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,3 @@
-/*var script = document.createElement('script');
-script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
-*/
 function includeScript(Source)
 {
 	var script = document.createElement('script');
@@ -12,66 +7,9 @@ function includeScript(Source)
 
 }
 
-//includeScript('https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js');
 includeScript('./scripts/parser.js');
 includeScript('./scripts/processing.js');
-//includeScript('./scripts/brailleTiles.js');
 
-
-
-let lookup = {
-	"0" : "a",
-	"01" : "b",
-	"03" : "c",
-	"034" : "d",
-	"04" : "e",
-	"013" : "f",
-	"0134" : "g",
-	"014" : "h",
-	"13" : "i",
-	"134" : "j",
-
-	"02" : "k",
-	"012" : "l",
-	"023" : "m",
-	"0234" : "n",
-	"024" : "o",
-	"0123" : "p",
-	"01234" : "q",
-	"0124" : "r",
-	"123" : "s",
-	"1234" : "t",
-
-	"025" : "u",
-	"0125" : "v",
-	"1345" : "w",
-	"0235" : "x",
-	"02345" : "y",
-	"0245" : "z",
-
-	
-	"1" : ",",
-	"12" : ";",
-	"14" : ":",
-	"145" : ".",
-	"124" : "!",
-	"1245" : "()",
-	"125" : "?\"", //quote start
-	"24" : "*",
-	"245" : "\"", //quote end
-	"2" : "\'",
-	"25" : "-",
-
-
-
-
-
-	"" : " "
-
-
-}
-
-var text = "";
 var activeTiles = [];
 
 class tile{
@@ -169,37 +107,7 @@ function updateText(text)
 	$("#lblOutput").text(text);
 }
 
-//remove
-function textLookup(dotinput)
-{
-	text = "";
-	letterCheck = lookup[dotinput];
-	if (letterCheck == undefined){
-		text = text;
-	}else
-	{
-		text = text + letterCheck;
-	}
 
-	return text
-}
-
-//remove
-function getActiveDotsForCurrentTile(tileId)
-{	
-	//get active dots for current tile and convert active to numbers
-	
-	var activeDotsInCurrentTileSet = "";
-	
-	for (var y = 0;y <= 5;y++)
-	{
-		if (activeTiles[tileId].getactiveDots(y) == true)
-			activeDotsInCurrentTileSet = activeDotsInCurrentTileSet + y; //+ y + " ";
-	}
-
-
-	return activeDotsInCurrentTileSet
-}
 
 
 //keep possibly add to the class
@@ -224,47 +132,10 @@ function getActiveDotsForCurrentTileBinary(tileId)
 		}
 	}
 
-
 	return activeDotsInCurrentTileSet
 }
 
 
-
-//remove
-function getAllActiveDots()
-{
-	var returnText =""
-	for (var i = 0; i <= activeTiles.length -1 ; i++) 
-	{
-		returnText += textLookup(getActiveDotsForCurrentTile(i));
-
-	}
-	updateText(returnText);
-}
-
-
-//remove
-function getAllDotValues()
-{	
-	text = "";
-	//get tile id, get dot array, output to a string
-	for (var i = 0; i <= activeTiles.length -1 ; i++) {
-		var activeDotsInCuttentTile = "";
-
-		for (var y = 0;y <= 5;y++){
-			if (activeTiles[i].getactiveDots(y) == true)
-				activeDotsInCuttentTile = activeDotsInCuttentTile + y; //+ y + " ";
-		}
-		letterCheck = lookup[activeDotsInCuttentTile];
-		
-		text = activeDotsInCuttentTile;
-		
-		
-		
-	}
-	
-	$("#lblOutput").text(text);
-}
 
 //keep
 function getDotBinary()
@@ -280,7 +151,6 @@ function getDotBinary()
 
 	}
 	return returnText
-	updateText(returnText);
 
 }
 
@@ -289,15 +159,11 @@ function checkboxUpdate()
 {
 	if($("#dbgGetDpts").is(":checked"))
 	{
-		//getAllDotValues();
-		getDotBinary();
+		updateText(getDotBinary());
 	}
 	else
 	{
-		//var binaryString = 
 		updateText(mainProcess(getDotBinary()))
-		//mainProcess(getDotBinary)
-		//getAllActiveDots()
 	}
 
 }
